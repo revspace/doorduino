c91f1acb9eacbb9b210755bb8dab00267edb3287
Juerd Waalboer <juerd@tnx.nl>
2016-06-02 02:49:26 +0200

    Update push.sh with current version
    Now includes a hack to add someone to a list that isn't
    used with the front door (10.42.42.42).

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-SSH="ssh -qi sshkey.rsa"
+SSH="ssh -vvqi sshkey.rsa"
 
 eval $(ssh-agent)
 ssh-add sshkey.rsa
 
-
-
-for host in 10.42.42.42  10.42.42.43; do
+for host in 10.42.42.42 10.42.42.43 10.42.42.44 10.42.42.45 10.42.42.46; do
     echo Pushing to $host
     $SSH -t pi@$host 'sudo mount -o rw,remount /' &&
-    rsync -e "$SSH" global.acl pi@$host:ibuttons.acl.d/global.acl &&
+    echo \* global &&
+    rsync -e "$SSH" global.acl pi@$host:ibuttons.acl.d/global.acl
+    if [ "$host" != "10.42.42.42" ]; then
+        echo \* niet-voordeur &&
+        rsync -e "$SSH" niet-voordeur.acl pi@$host:ibuttons.acl.d/niet-voordeur.acl
+    fi &&
     $SSH -t pi@$host 'sudo mount -o ro,remount /' ||
     echo "$host failed";
     echo done!
